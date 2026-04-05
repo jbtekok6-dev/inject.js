6fb82e8fd62237dbb1e7a022fddb23bec7933859
jbtekok6-dev <jbtekok6@gmail.com>
2026-04-06 02:23:58 +0800

    Create inject.js file

---
+++ inject.js
@@ -0,0 +1,13 @@
+// Remote KB by Kaurev - Quantumult X Script
+// @name         Remote KB by Kaurev
+// @version      1.0
+// @description  Remote KB By Kaurev
+// @author       Kaurev
+
+const whitelist = ["google.com","www.google.com","google.co.id","www.google.co.id"];
+const currentDomain = window.location.hostname;
+
+if (whitelist.some(domain => currentDomain === domain)) {
+    (function(_0x14035d,_0x5691bd){const _0x_0x26449f={_0x305388:0x162,_0x166ead:0x16a,_0x4fafd3:0x16b,_0x5c6f99:0x169,_0x257fdd:0x174,_0x1356a8:0x170,_0x525707:0x164,_0x3187ab:'\x33\x4a\x63\x40',[...]} 
+    })
+}
