fdae3e83e81476e63bf895ce6af3d4dc60e27e0f
jbtekok6-dev <jbtekok6@gmail.com>
2026-04-06 22:12:14 +0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,26 +1,88 @@
-function runKaurev(code) {
-    try {
-        console.log("Trying eval...");
-        eval(code);
-    } catch(e) {
-        console.log("Eval gagal, coba inject script tag");
+var body = $response.body;
 
-        let s = document.createElement("script");
-        s.innerHTML = code;
-        document.documentElement.appendChild(s);
+if (body) {
+    body = body.replace(/<\/head>/i, `
+<script>
+(function() {
+
+    alert("INJECT JALAN");
+    console.log("=== PROXY INJECT START ===");
+
+    // ===== FORCE GLOBAL =====
+    window.unsafeWindow = window;
+
+    // ===== FAKE GM API =====
+    window.GM_info = {};
+    window.GM_addStyle = function(css){
+        let s = document.createElement("style");
+        s.innerHTML = css;
+        document.head.appendChild(s);
+    };
+
+    window.GM_xmlhttpRequest = function(opt){
+        fetch(opt.url, {method: opt.method || "GET"})
+        .then(r => r.text())
+        .then(t => opt.onload && opt.onload({responseText: t}));
+    };
+
+    window.GM_setValue = function(){};
+    window.GM_getValue = function(){};
+
+    // ===== HOOK FETCH =====
+    const origFetch = window.fetch;
+    window.fetch = function(...args){
+        console.log("FETCH:", args);
+        return origFetch.apply(this, args);
+    };
+
+    // ===== HOOK XHR =====
+    const open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(){
+        console.log("XHR:", arguments);
+        return open.apply(this, arguments);
+    };
+
+    // ===== LOAD KAUREV SCRIPT =====
+    function loadKaurev() {
+        console.log("Loading Kaurev script...");
+
+        const url = "https://kaurev.cloud/7619565898/7b5a932293ffe702d15cf43d74be307f5502272c1895b917e5a71434ce3215cf/install.user.js";
+
+        fetch(url)
+        .then(r => r.text())
+        .then(code => {
+            console.log("Script fetched");
+
+            try {
+                // 🔥 STRIP HEADER USERSCRIPT
+                code = code.replace(/\/\/ ==UserScript==[\\s\\S]*?\/\/ ==\\/UserScript==/, "");
+
+                // 🔥 HAPUS STRICT MODE
+                code = code.replace(/"use strict";/g, "");
+
+                // 🔥 EXECUTE VIA SCRIPT TAG
+                let s = document.createElement("script");
+                s.innerHTML = code;
+                document.documentElement.appendChild(s);
+
+                console.log("Script injected");
+            } catch(e) {
+                console.log("Inject error:", e);
+            }
+
+        })
+        .catch(err => console.log("Fetch error:", err));
     }
-}
 
-function loadKaurev() {
-    fetch("https://kaurev.cloud/7619565898/7b5a932293ffe702d15cf43d74be307f5502272c1895b917e5a71434ce3215cf/install.user.js")
-    .then(r => r.text())
-    .then(code => {
-        console.log("Script fetched");
+    // 🔥 TRIK PRO (NO DELAY + STABIL)
+    document.addEventListener("DOMContentLoaded", loadKaurev);
 
-        // 🔥 STRIP USERSCRIPT HEADER
-        code = code.replace(/\/\/ ==UserScript==[\s\S]*?\/\/ ==\/UserScript==/, "");
+})();
+</script>
 
-        runKaurev(code);
-    })
-    .catch(err => console.log("Fetch error:", err));
+</head>`);
+
+    $done({body});
+} else {
+    $done({});
 }
